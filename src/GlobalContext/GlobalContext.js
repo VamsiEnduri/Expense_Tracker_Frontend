d9aed5dd0c23ce3d2025050e13376346e778daa9
VamsiEnduri <vamsienduri1310@gmail.com>
2024-03-26 16:15:21 +0530

    added backend render link to contect file as base url

--- src/GlobalContext/GlobalContext.js
+++ src/GlobalContext/GlobalContext.js
@@ -1,7 +1,7 @@
 import React, { useContext, useState, createContext } from "react";
 import axios from "axios";
 
-const BASE_URL = "https://mern-expense-tracker-3-backend-api.onrender.com/";
+const BASE_URL = "https://expense-tracker-backend-api.onrender.com/";
 
 const GlobalContext = createContext();
 
